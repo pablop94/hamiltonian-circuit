421d1ebe11e38fbf13c07a80b6f1ae26c6ce9c28
Pablo Pissi <pablopissi@gmail.com>
2023-06-26 01:06:11 -0300

    Quito comentario

--- generador.py
+++ generador.py
@@ -5,7 +5,6 @@ import random
 def generar_grafo(n, max_cost):
     matriz_costos = [[random.randint(1, max_cost) for _ in range(n)] for _ in range(n)]
 
-    # Asignar un valor alto a la diagonal principal para evitar ciclos
     for i in range(n):
         matriz_costos[i][i] = 0
 
