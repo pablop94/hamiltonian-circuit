3e49adaede07fb8c1fe801f0f1cc85e8820e70f5
Pablo Pissi <pablopissi@gmail.com>
2023-06-26 23:50:57 -0300

    Renombro variable

--- repartidor_03.py
+++ repartidor_03.py
@@ -6,21 +6,21 @@ def busqueda_local(grafo, solucion):
 
     posible_mejora = solucion.copy()
     fin_anterior = None
-    next_index = 0
+    proximo_indice = 0
     for i, (inicio, fin) in enumerate(solucion):
-        if fin_anterior is not None and i > next_index:
+        if fin_anterior is not None and i > proximo_indice:
             posible_mejora[i-1] = posible_mejora[i-1][0], fin
             posible_mejora[i] = fin, fin_anterior
-            next_index = (i+1) % len(posible_mejora)
-            posible_mejora[next_index] = fin_anterior, posible_mejora[next_index][1]
+            proximo_indice = (i+1) % len(posible_mejora)
+            posible_mejora[proximo_indice] = fin_anterior, posible_mejora[proximo_indice][1]
             nuevo_costo = costo_camino(grafo, posible_mejora)
             if costo_actual > nuevo_costo:
                 costo_actual = nuevo_costo
             else:
                 posible_mejora[i-1] = solucion[i-1]
                 posible_mejora[i] = solucion[i]
-                posible_mejora[next_index] = solucion[next_index]
-                next_index = i
+                posible_mejora[proximo_indice] = solucion[proximo_indice]
+                proximo_indice = i
         fin_anterior = fin
 
     return posible_mejora
